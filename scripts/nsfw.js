41ca2b72766aff377d4f3b9942b181b609e41564
Paul Lambert <paul@notpml.com>
2012-04-29 19:34:27 -0500

    adding search functionality based on clickable links

--- scripts/nsfw.js
+++ scripts/nsfw.js
@@ -1,4 +1,4 @@
-function search_for(var querystring) {
+function search_for(querystring) {
   var SEARCH_URL = "/search";
 
   $.ajax({
@@ -11,11 +11,47 @@ function search_for(var querystring) {
     error: function(errorData) {
       alert("error");
       console.log(errorData);
+      return errorData;
     },
 
     success: function(data) { 
       alert("success");
       console.log(data);
+      return data;
     }
   });
+}
+
+function loadMid(id) {
+  var data;
+
+  switch(id) { 
+
+    // nudity
+    case '1':
+      data = search_for("boobies|penis|boobs|dick|vagina|twat|tit|porn|nude|naked|pussy|boner");
+      break;
+
+    // profanity
+    case '2':
+      data = search_for("fuck|ass|shit|bitch");
+      break;
+
+    // xenophobia
+    case '3':
+      data = search_for("fags|niggers|chink|beaner|gay|homo|xenophobia");
+      break;
+
+    // misogyny
+    case '4':
+      data = search_for("bitch|cunt|ho|slut|skank|misogyny");
+      break;
+
+    // nsfw & nsfwl
+    case '5':
+      data = search_for("nsfw | nsfl");
+      break;
+  }
+
+  console.log(data);
 }
