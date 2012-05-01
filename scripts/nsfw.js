aa0d6d7705060d64c1b285fa7a4b74cce4299277
Paul Lambert <paul@notpml.com>
2012-04-30 22:01:48 -0500

    data acquisition underway

--- scripts/nsfw.js
+++ scripts/nsfw.js
@@ -23,35 +23,57 @@ function search_for(querystring) {
 }
 
 function loadMid(id) {
-  var data;
+  var querystring = "";
 
   switch(id) { 
 
     // nudity
     case '1':
-      data = search_for("boobies|penis|boobs|dick|vagina|twat|tit|porn|nude|naked|pussy|boner");
+      // querystring = "boobies|penis|boobs|dick|vagina|twat|tit|porn|nude|naked|pussy|boner";
+      querystring = "pussy";
       break;
 
     // profanity
     case '2':
-      data = search_for("fuck|ass|shit|bitch");
+      querystring = "fuck|ass|shit|bitch";
       break;
 
     // xenophobia
     case '3':
-      data = search_for("fags|niggers|chink|beaner|gay|homo|xenophobia");
+      querystring = "fag|nigger|chink|beaner|gay|homo|xenophobia|terrorist|jap";
       break;
 
     // misogyny
     case '4':
-      data = search_for("bitch|cunt|ho|slut|skank|misogyny");
+      querystring = "bitch\|cunt\|ho\|slut\|skank\|misogyny";
       break;
 
     // nsfw & nsfwl
     case '5':
-      data = search_for("nsfw | nsfl");
+      querystring = "nsfw | nsfl";
       break;
   }
 
-  console.log(data);
+  var SEARCH_URL = "/search";
+
+  $.ajax({
+    url: SEARCH_URL,
+    data: {
+      query:querystring,
+      // limit:"10"
+    },
+    async: false,
+
+    error: function(errorData) {
+      alert("error");
+      console.log(errorData);
+      return errorData;
+    },
+
+    success: function(data) { 
+      alert("success");
+      console.log(data);
+      return data;
+    }
+  });
 }
