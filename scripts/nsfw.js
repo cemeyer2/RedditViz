6cc6ab93aba35aa269be6fe06321c54acf978958
Paul Lambert <paul@notpml.com>
2012-04-30 23:57:14 -0500

    oh what the fuck

--- scripts/nsfw.js
+++ scripts/nsfw.js
@@ -22,33 +22,44 @@ function search_for(querystring) {
   });
 }
 
+
 function loadMid(id) {
   var querystring = "";
 
-  var presentation = true;
-
+  var presentation;
   if(presentation) { 
     switch(id) { 
-
       // nudity
       case '1':
+        console.log(nudity_boobies);
+        console.log(nudity_dick);
+        console.log(nudity_pussy);
         break;
 
       // profanity
       case '2':
+        console.log(profanity_fuck);
+        console.log(profanity_shit);
+        console.log(profanity_ass);
         break;
 
       // xenophobia
       case '3':
+        console.log(xeno_faggot);
+        console.log(xeno_nigger);
         break;
 
       // misogyny
       case '4':
+        console.log(misogyny_cunt);
+        console.log(misogyny_slut);
+        console.log(misogyny_bitch);
         break;
-
 
       // nsfw & nsfl
       case '5':
+        console.log(nsf_nsfw);
+        console.log(nsf_nsfl);
         break;
     }
   }
@@ -80,28 +91,28 @@ function loadMid(id) {
         querystring = "nsfw | nsfl";
         break;
     }
+
+    var SEARCH_URL = "/search";
+  
+    $.ajax({
+      url: SEARCH_URL,
+      data: {
+        query:querystring,
+        // limit:"10"
+      },
+      async: false,
+  
+      error: function(errorData) {
+        alert("error");
+        console.log(errorData);
+        return errorData;
+      },
+  
+      success: function(data) { 
+        alert("success");
+        console.log(data);
+        return data;
+      }
+    });
   }
-
-  var SEARCH_URL = "/search";
-
-  $.ajax({
-    url: SEARCH_URL,
-    data: {
-      query:querystring,
-      // limit:"10"
-    },
-    async: false,
-
-    error: function(errorData) {
-      alert("error");
-      console.log(errorData);
-      return errorData;
-    },
-
-    success: function(data) { 
-      alert("success");
-      console.log(data);
-      return data;
-    }
-  });
 }
